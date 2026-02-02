07266fbed18ab485ece2016cde2c3a73dbe1b5ac
Filip Hjerten <f.hjerten@gmail.com>
2026-02-02 18:33:20 +0100

    update

---
+++ data_loader.py
@@ -0,0 +1,2 @@
+import pandas as pd
+from team import Team
---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+
+    def __init__(self, name, position, gender, number, stats):
+        self.name = name
+        self.position = position
+        self.gender = gender
+        self.number = number
+        self.stats = stats
+
+    
